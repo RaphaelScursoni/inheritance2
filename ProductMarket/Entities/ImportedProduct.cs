6f1890822c963b9e7b76f4a818262d6374eae5b5
Raphael Scursoni <ra.scursoni@gmail.com>
2020-09-16 21:50:16 -0300

    Training with inheritance

---
+++ ProductMarket/Entities/ImportedProduct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductMarket.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct() { }
+
+        public ImportedProduct(string name, double price, double customsFee) 
+            : base (name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice(double price, double customsFee) 
+        {
+            double p = price + customsFee;
+            return p;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice(Price, CustomsFee)
+                + " "
+                + "(Customs fee: $ "
+                + CustomsFee
+                + ")";
+        }
+    }
+}
---
+++ ProductMarket/Entities/UsedProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductMarket.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct () { }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate) 
+            : base (name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public override string PriceTag()
+        {
+            return Name
+                + " (Used)"
+                + " $ "
+                + Price
+                + " (Manufacture date: "
+                + ManufactureDate
+                + ")";
+        }
+    }
+}
